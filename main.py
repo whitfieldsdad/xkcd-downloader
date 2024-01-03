f80ff87a9b617fdbea45f924af88b5c2361abcd8
Tyler Fisher <tylerfisher@tylerfisher.ca>
2024-01-03 01:10:32 -0500

    Cache list of download URLs

--- main.py
+++ main.py
@@ -6,29 +6,24 @@ from urllib.request import urlopen
 import concurrent.futures
 import logging
 import datetime
+import tempfile
 import json
+import os
 
 logger = logging.getLogger(__name__)
 
-INDEX_URL = "https://xkcd.com/info.0.json"
+CACHE_DIR = os.path.join(tempfile.gettempdir(), "xkcd")
+URL_CACHE_PATH = os.path.join(CACHE_DIR, "urls.json")
 
 
 def main(output_dir: str, force: bool = False):
-    total = get_total_comics()
-    logger.info("Found %d comics", total)
-
-    logger.info("Finding download urls...")
-    start_time = datetime.datetime.now()
-    urls = tuple(iter_download_urls(total))
-    end_time = datetime.datetime.now()
-    logger.info("Found %d download urls in %.2f seconds", len(urls), (end_time - start_time).total_seconds())
-
     pending = {}
+    urls = list(iter_download_urls())
     for url in urls:
         path = os.path.join(output_dir, os.path.basename(url))
         if not os.path.exists(path) or force:
             pending[url] = path
-    
+
     if not pending:
         logger.info("All %d comics have been downloaded", len(urls))
     else:
@@ -49,7 +44,7 @@ def main(output_dir: str, force: bool = False):
 
 
 def download(url: str, path: str):
-    logger.debug("Downloading %s -> %s", url, path)
+    logger.info("Downloading %s -> %s", url, path)
     try:
         with open(path, "wb") as file:
             file.write(urlopen(url).read())
@@ -57,34 +52,69 @@ def download(url: str, path: str):
         logger.error("Failed to download %s -> %s - %s", url, path, e)
         return
     else:
-        logger.debug("Downloaded %s -> %s", url, path)
+        logger.info("Downloaded %s -> %s", url, path)
 
 
-def iter_download_urls(total: Optional[int] = None) -> Iterator[str]:
-    def _get_download_url(idx: int) -> str:
-        return json.loads(urlopen(f"https://xkcd.com/{idx}/info.0.json").read())["img"]
+def iter_download_urls() -> Iterator[str]:
+    def resolve_download_url(url: str) -> str:
+        return json.loads(urlopen(url).read())["img"]
 
-    total = total or get_total_comics()
+    total = get_total_comics()
+    cache = read_url_cache()
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = []
-        for i in reversed(range(1, total + 1)):
-            future = executor.submit(_get_download_url, i)
-            futures.append(future)
+        futures = {}
+        for i in range(1, total + 1):
+            api_url = f"https://xkcd.com/{i}/info.0.json"
+            img_url = cache.get(api_url)
+            if img_url:
+                cache[api_url] = img_url
+                yield img_url
+            else:
+                future = executor.submit(resolve_download_url, api_url)
+                futures[future] = api_url
+        
+        if futures:
+            for future in concurrent.futures.as_completed(futures):
+                api_url = futures[future]
+                try:
+                    img_url = future.result()
+                except HTTPError as e:
+                    if e.code == 404:
+                        continue
+                    raise
+                else:
+                    cache[api_url] = img_url
+                    yield img_url
 
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                yield future.result()
-            except HTTPError:
-                continue
+    update_url_cache(cache)
 
 
 def get_total_comics() -> int:
-    data = json.loads(urlopen(INDEX_URL).read())
+    data = json.loads(urlopen("https://xkcd.com/info.0.json").read())
     return data["num"]
 
 
-if __name__ == "__main__":
+def read_url_cache(path: Optional[str] = URL_CACHE_PATH) -> dict:
+    try:
+         with open(path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+    except json.decoder.JSONDecodeError:
+        os.unlink(path)
+        return {}
 
+
+def update_url_cache(cache: dict, path: Optional[str] = URL_CACHE_PATH):
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
+
+    with open(path, "w") as file:
+        json.dump(cache, file)
+
+
+if __name__ == "__main__":
     def cli():
         import argparse
 
@@ -95,8 +125,12 @@ if __name__ == "__main__":
             "-o", "--output-dir", help="Output directory", required=True
         )
         parser.add_argument("-f", "--force", action="store_true", help="Force download")
+        parser.add_argument('-v', '--verbose', action='store_true', help='Verbose logging')
         args = parser.parse_args()
         kwargs = vars(args)
+        if kwargs.pop("verbose"):
+            logging.getLogger().setLevel(logging.DEBUG)
+
         main(**kwargs)
 
     cli()
